f4a1498f88898d23ff11569b79c05b80b631ea43
Elijah Arhinful <elijaharhinful8@gmail.com>
2024-06-28 11:25:32 +0000

    added mailing functionality to contact section

--- mail.php
+++ mail.php
@@ -1,26 +1,26 @@
 <?php
-$name = $_POST["name"];
-$email = $_POST["email"];
-$message = $_POST["message"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"]);
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $message = htmlspecialchars($_POST["message"]);
 
+    $EmailTo = "elijaharhinful8@gmail.com";
+    $Subject = "New Message Received From Edna Frimpong Website";
 
-$EmailTo = "000.lunchbox@gmail.com";
-$Subject = "New Message Received";
+    // prepare email body text
+    $Fields = "Name: $name\n";
+    $Fields .= "Email: $email\n";
+    $Fields .= "Message: $message\n";
 
-// prepare email body text
-$Fields .= "Name: ";
-$Fields .= $name;
-$Fields .= "\n";
+    // send email
+    $success = mail($EmailTo, $Subject, $Fields, "From:" . $email);
 
-$Fields.= "Email: ";
-$Fields .= $email;
-$Fields .= "\n";
-
-$Fields .= "Message: ";
-$Fields .= $message;
-$Fields .= "\n";
-
-
-// send email
-$success = mail($EmailTo,  $Subject,  $Fields, "From:".$email);
-
+    if ($success) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+} else {
+    echo "Invalid request method.";
+}
+?>
